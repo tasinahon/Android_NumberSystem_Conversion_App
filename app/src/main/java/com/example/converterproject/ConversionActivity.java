9f1bca006ba4cfdc2e0eec3eec7c7e412ebc207d
tasinashon <01788497275ahon1984@gmail.com>
2024-04-20 02:46:03 +0600

    all

--- app/src/main/java/com/example/converterproject/ConversionActivity.java
+++ app/src/main/java/com/example/converterproject/ConversionActivity.java
@@ -1,8 +1,10 @@
 package com.example.converterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,9 +14,10 @@ import android.widget.Toast;
 
 public class ConversionActivity extends AppCompatActivity {
     String[] item={"Decimal","Binary","Octal","Hexadecimal","Others"};
-
+    String item1,item2;
     AutoCompleteTextView autoCompleteTextView,autoCompleteTextView1;
     ArrayAdapter<String>adapteritems;
+    ConstraintLayout done;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +30,27 @@ public class ConversionActivity extends AppCompatActivity {
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item=adapterView.getItemAtPosition(i).toString();
-                Toast.makeText(ConversionActivity.this,"Item: "+item,Toast.LENGTH_SHORT).show();
+                item1=adapterView.getItemAtPosition(i).toString();
+
             }
         });
         autoCompleteTextView1=findViewById(R.id.auto_complete_text1);
         autoCompleteTextView1.setAdapter(adapteritems);
+        autoCompleteTextView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                item2=adapterView.getItemAtPosition(i).toString();
+
+            }
+        });
+        done = findViewById(R.id.done);
+        done.setOnClickListener((view -> {
+            Intent intent=new Intent(this,InpActivity.class);
+            intent.putExtra("From",item1);
+            intent.putExtra("To",item2);
+            startActivity(intent);
+
+        }));
 
 
     }
---
+++ app/src/main/java/com/example/converterproject/InpActivity.java
@@ -0,0 +1,43 @@
+package com.example.converterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class InpActivity extends AppCompatActivity {
+    String from,to;
+    TextView textViewfrom,textViewto;
+    ConstraintLayout ok;
+    TextView req,totext;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inp);
+        Intent intent=getIntent();
+        from=intent.getStringExtra("From");
+        to=intent.getStringExtra("To");
+        textViewfrom=findViewById(R.id.fromt);
+        textViewfrom.setText(from);
+
+        req=findViewById(R.id.reqvalue);
+        totext=findViewById(R.id.totext);
+        totext.setText(to);
+        ok=findViewById(R.id.ok);
+        ok.setOnClickListener((view -> {
+            req.setVisibility(View.VISIBLE);
+            totext.setVisibility(View.VISIBLE);
+
+        }));
+
+
+
+
+    }
+}
